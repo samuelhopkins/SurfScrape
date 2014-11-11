84858864ee6f270608be894c1136f1f834fd86b9
Sam Hopkins <safhopkins@yahoo.com>
2014-11-10 19:23:32 -0600

    job scheduled to run once a day at 10am central

--- surf.py
+++ surf.py
@@ -1,7 +1,13 @@
 from bs4 import BeautifulSoup
 import urllib2
 import smtplib
+import datetime
+import time
+from apscheduler.scheduler import Scheduler
 
+sched = Scheduler()
+sched.daemonic=False
+sched.start()
 
 def scrape():
 	search_url="http://www.surfline.com/surf-report/pleasure-point-central-california_4190/"
@@ -13,27 +19,34 @@ def scrape():
 	spot_conditions=soup.find('div',{'id': 'observed-spot-conditions'})
 	conditions=spot_conditions.text
 
-	
-	print wave_height[:1]
+	if wave_height[:1]>=5:
+		return True, conditions, wave_height
 	if "good" in conditions:
-		return True
+		return True, conditions, wave_height
 	if "fair" in conditions and wave_height[:1]>=3:
-		return True
-	return False
+		return True, conditions, wave_height
+	return False,conditions,wave_height
 			
 
 
 
-def main():
+def send_message():
 	server = smtplib.SMTP( "smtp.gmail.com", 587 )
 	server.starttls()
 	server.login( <your email>, <password> )
-	go_surfing=scrape()
-
+	go_surfing,conditions,wave_height=scrape()
+	conditions=conditions.replace("Conditions","")
+	message= "You should go surfing today. The conditions at Pleasure Point are "+ conditions+ "with a wave height of "+wave_height
+	print message
 	if go_surfing:
-		server.sendmail( 'Sam', <your number>@mms.att.net', 'Go surfing today!' )
+		server.sendmail( 'Sam', <your number>@mms.att.net, message)
+		server.sendmail( 'Sam', <a friends number>@mms.att.net, message)
 
 	print "Message sent"
+
+def main():
+	job=sched.add_interval_job(send_message,minutes=0.2,start_date='2014-11-11 10:00:00', args="")
+
 
 if __name__=="__main__":
 	main()
